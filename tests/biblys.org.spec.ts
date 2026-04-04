0171e753c73b185242e603de11eb7b4c6c8a8e0d
Clément Latzarus <c@ltzr.io>
2026-04-04 11:46:26 +0200

    tests: add E2E tests for every pages

--- tests/biblys.org.spec.ts
+++ tests/biblys.org.spec.ts
@@ -1,7 +1,62 @@
-import { test, expect } from '@playwright/test';
+import { expect, type Page, test } from '@playwright/test';
 
 test('home page', async ({ page }) => {
   await page.goto('/');
 
   await expect(page).toHaveTitle('Biblys · Le logiciel libre pour créer des boutiques de livre en ligne.');
 });
+
+test('tarifs', async ({ page }) => {
+  await page.goto('/');
+
+  await openMobileMenu(page);
+  await page.click('text=Tarifs');
+
+  await expect(page).toHaveTitle('Tarifs · Biblys');
+});
+
+test('faq', async ({ page }) => {
+  await page.goto('/');
+
+  await openMobileMenu(page);
+  await page.click('text=FAQ');
+
+  await expect(page).toHaveTitle('Questions fréquentes · Biblys');
+});
+
+test('blog', async ({ page }) => {
+  await page.goto('/');
+
+  await openMobileMenu(page);
+  await page.click('text=Blog');
+
+  await expect(page).toHaveTitle('Blog · Biblys');
+});
+
+test('contact', async ({ page }) => {
+  await page.goto('/');
+
+  await openMobileMenu(page);
+  await page.click('text=Contact');
+
+  await expect(page).toHaveTitle('Contact · Biblys');
+});
+
+test('commencer', async ({ page }) => {
+  await page.goto('/');
+
+  await openMobileMenu(page);
+  await page.click('text=Commencer');
+
+  await expect(page).toHaveTitle('Commencer · Biblys');
+});
+
+function isMobile(page: Page) {
+  return page.viewportSize()!.width < 768;
+}
+
+async function openMobileMenu(page: Page) {
+  if (isMobile(page)) {
+    await page.getByRole('button', { name: 'Toggle Menu' }).click();
+  }
+}
